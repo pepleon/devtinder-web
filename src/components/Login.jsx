e3181888ae81b56810577d472c0ee0510589c1cb
pepleon <amanhusainbutt@gmail.com>
2025-01-24 19:08:21 +0530

    login improved

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,8 +7,8 @@ import { BASE_URL } from '../utils/constants';
 
 
 const Login = () => {
- const [emailId, setEmailId] = useState("@gmail.com");
- const [password, setPassword] = useState("Aman@123");
+ const [emailId, setEmailId] = useState("");
+ const [password, setPassword] = useState("");
  const [error, setError] = useState("");
  const [firstName, setFirstName] = useState("");
  const [lastName, setLastName] = useState("");
